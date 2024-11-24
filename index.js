a3e18104a89fd6579cd35455cfdabd5685f2c0b0
nagarjunaprasad98 <nagarjunaprasad99@gmail.com>
2024-11-24 21:18:29 +0530

    updates

--- index.js
+++ index.js
@@ -9,8 +9,32 @@ function downloadPDF() {
 }
 
 function openPDF() {
-    const link = document.createElement("a");
-    link.href = "/assests/Nagarjuna'sCV.pdf"; // Path to your PDF file
-    link.target = "_blank"; // Opens the PDF in a new tab
-    link.click();
-  }
+  const link = document.createElement("a");
+  link.href = "/assests/Nagarjuna'sCV.pdf"; // Path to your PDF file
+  link.target = "_blank"; // Opens the PDF in a new tab
+  link.click();
+}
+
+// Wait for the DOM content to load
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all nav links
+  const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+  // Add event listener to each nav link
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      // Remove 'active' class from all nav items
+      document.querySelectorAll(".navbar-nav .nav-item").forEach((item) => {
+        item.classList.remove("active");
+      });
+
+      // Add 'active' class to the clicked nav item
+      this.parentElement.classList.add("active");
+    });
+  });
+});
+
+window.addEventListener('load', function() {
+    const navbarHeight = document.querySelector('.navbar').offsetHeight;
+    document.querySelector('.jumbotron').style.paddingTop = navbarHeight + 'px';
+});
